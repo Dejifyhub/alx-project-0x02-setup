5c56a182526eb4a24be4262e5629b6bb23046f82
Thexthnksdnm <169040134+Thexthnksdnm@users.noreply.github.com>
2025-09-22 06:15:27 -0500

    Commit

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Header from "@/components/layout/Header";
 import Card from "@/components/common/Card";
-import PostModal from "@/components/common/Postmodal";
+import PostModal from "@/components/common/PostModal";
 
 interface Post {
   title: string;
